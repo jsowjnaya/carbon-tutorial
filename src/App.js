bc5d5940ca9c4c032c65407ab1394cfc2fbae539
Sowjanya Jagathapu <93701836+jsowjnaya@users.noreply.github.com>
2021-11-04 19:36:25 +0530

    step-1 app changes
    committed step-1 app changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.scss';
+import { Button, Content } from 'carbon-components-react';
+import TutorialHeader from './components/TutorialHeader';
+//import { Route, Switch } from 'react-router-dom';
+//import {Route,BrowserRouter,Switch} from "react-router-dom";
+//import { Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Link } from "react-router-dom";
+//import { Route, Switch } from 'react-router-dom';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
+
+
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
-      </div>
+      /*<div>
+        Hello Carbon! Well, not quite yet. This is the starting point for the
+        Carbon tutorial.
+      </div>*/
+	  //<Button>Button</Button>
+	  
+	        <>
+        <TutorialHeader />
+        <Content>
+		  <Switch>
+  <Route exact path="/" component={LandingPage} />
+  <Route path="/repos" component={RepoPage} />
+</Switch>
+        </Content>
+      </>
     );
   }
 }
 
 export default App;
+
